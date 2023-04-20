aa810b78e07d1b827c13c9989b0751d2eb1e48ef
Nasa Dheeraj <dheeraj6082@gmail.com>
2023-04-20 16:31:02 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.get(`/`,(req,res)=>{
+res.json("API Running")
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT,()=>{
+    console.log(`Server started port ${PORT}`);
+});
